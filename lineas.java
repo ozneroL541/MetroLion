c4c90a503c58ab1aff3efe0a31c4b6df97183875
ozneroL541 <l.21radice@gmail.com>
2023-11-23 12:29:44 +0100

    line A

--- lineas.java
+++ lineas.java
@@ -1,5 +1,29 @@
 
 
 public class lineas {
-        
+    private final class line_num {
+        short A = 0;
+        short B = 1;
+        short C = 2;
+        short D = 3;
+    }
+    private final String[][] linea = {
+        {       
+                "Vaulx-en-Velin - La Soie\n" + //
+                "Cusset\n" + //
+                "Flachet - Alain Gilles\n" + //
+                "Gratte-Ciel\n" + //
+                "République - Villeurbanne\n" + //
+                "Charpennes - Charles Hernu\n" + //
+                "Masséna\n" + //
+                "Foch\n" + //
+                "Hôtel de Ville\n" + //
+                "Cordeliers\n" + //
+                "Bellecour\n" + //
+                "Ampère - Victor Hugo\n" + //
+                "Perrache"
+            },
+        {},
+        {}
+    }
 }
