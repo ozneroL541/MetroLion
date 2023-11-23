5f35c1f39b46c2cc717b1971d8350ff0fc00cb56
ozneroL541 <l.21radice@gmail.com>
2023-11-23 12:31:01 +0100

    linea A

--- lineas.java
+++ lineas.java
@@ -9,20 +9,20 @@ public class lineas {
     }
     private final String[][] linea = {
         {       
-                "Vaulx-en-Velin - La Soie\n" + //
-                "Cusset\n" + //
-                "Flachet - Alain Gilles\n" + //
-                "Gratte-Ciel\n" + //
-                "République - Villeurbanne\n" + //
-                "Charpennes - Charles Hernu\n" + //
-                "Masséna\n" + //
-                "Foch\n" + //
-                "Hôtel de Ville\n" + //
-                "Cordeliers\n" + //
-                "Bellecour\n" + //
-                "Ampère - Victor Hugo\n" + //
-                "Perrache"
-            },
+            "Vaulx-en-Velin - La Soie\n",
+            "Cusset\n",
+            "Flachet - Alain Gilles\n",
+            "Gratte-Ciel\n",
+            "République - Villeurbanne\n",
+            "Charpennes - Charles Hernu\n",
+            "Masséna\n",
+            "Foch\n",
+            "Hôtel de Ville\n",
+            "Cordeliers\n",
+            "Bellecour\n",
+            "Ampère - Victor Hugo\n",
+            "Perrache"
+        },
         {},
         {}
     }
