f66153810043c8aac1d8f9a1517b27df05bd545a
ozneroL541 <l.21radice@gmail.com>
2023-12-08 16:50:24 +0100

    A* algortithm

---
+++ alg_Astar.java
@@ -0,0 +1,92 @@
+import java.util.*;
+
+public class alg_Astar {
+    // TODO Adapt
+
+
+    public List<Station> reconstructPath(Map<Station, Station> cameFrom, Station current) {
+        List<Station> totalPath = new ArrayList<>();
+        totalPath.add(current);
+        while (cameFrom.containsKey(current)) {
+            current = cameFrom.get(current);
+            totalPath.add(0, current);
+        }
+        return totalPath;
+    }
+    /**
+     * A* finds a path from start to goal.
+     * h is the heuristic function.
+     * h(n) estimates the cost to reach goal from node n.
+     * @param start
+     * @param goal
+     * @param h
+     * @return list of stations
+     */
+    public List<Station> aStarSearch(Station start, Station goal, HeuristicFunction h) {
+        /*
+        * The set of discovered nodes that may need to be (re-)expanded.
+        * Initially, only the start node is known.
+        * This is usually implemented as a min-heap or priority queue rather than a hash-set.
+        */
+        Set<Station> openSet = new HashSet<>();
+        openSet.add(start);
+
+        /*
+         * For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from the start
+         * to n currently known
+         */
+        Map<Station, Station> cameFrom = new HashMap<>();
+        Map<Station, Double> gScore = new HashMap<>();
+        gScore.put(start, 0.0);
+
+        /*
+         * For node n, fScore[n] := gScore[n] + h(n). fScore[n] represents our current best guess as to
+         * how cheap a path could be from start to finish if it goes through n.
+         */
+        Map<Station, Double> fScore = new HashMap<>();
+        fScore.put(start, h.estimateCost(start, goal));
+
+        while (!openSet.isEmpty()) {
+            /*
+             * This operation can occur in O(Log(N)) time if openSet is a min-heap or a priority queue
+             */
+            Station current = openSet.stream()
+                    .min(Comparator.comparingDouble(fScore::get))
+                    .orElseThrow();
+
+            if (current.equals(goal)) {
+                return reconstructPath(cameFrom, current);
+            }
+
+            openSet.remove(current);
+
+            for (Station neighbor : current.getNearStations()) {
+                /*
+                 * d(current,neighbor) is the weight of the edge from current to neighbor
+                 * tentative_gScore is the distance from start to the neighbor through current
+                 */
+                double tentativeGScore = gScore.getOrDefault(current, Double.POSITIVE_INFINITY)
+                        + current.NearStationTime(neighbor);
+
+                if (tentativeGScore < gScore.getOrDefault(neighbor, Double.POSITIVE_INFINITY)) {
+                    /*
+                     * This path to neighbor is better than any previous one. Record it!
+                     */
+                    cameFrom.put(neighbor, current);
+                    gScore.put(neighbor, tentativeGScore);
+                    fScore.put(neighbor, tentativeGScore + h.estimateCost(neighbor, goal));
+                    openSet.add(neighbor);
+                }
+            }
+        }
+        /*
+         * Open set is empty but goal was never reached
+         */
+        return Collections.emptyList(); // No path found
+    }
+    public interface HeuristicFunction {
+        double estimateCost(Station node, Station goal);
+    }
+
+    // Define Station class and related methods as needed
+}
